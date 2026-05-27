a3126f7e761f8bc25210485c6d7904f40bbce544
Hunta <hunta@onycom.com>
2026-05-27 17:50:03 +0900

    fix: AuthFilter 加上 @Component 注解

--- gateway-service/src/main/java/com/onycom/gateway/filter/AuthFilter.java
+++ gateway-service/src/main/java/com/onycom/gateway/filter/AuthFilter.java
@@ -5,9 +5,11 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+@Component
 public class AuthFilter implements GlobalFilter, Ordered {
     private static final String LOGIN_PATH = "/api/auth/login";
 
